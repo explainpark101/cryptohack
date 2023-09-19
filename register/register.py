4222d413fdf237725b17339947984855091af601
Park Jaehyung <explainpark101@gmail.com>
2023-09-19 21:57:50 +0900

    Challenges 폴더 새성 및 Introduction 풀이 완료

--- register/register.py
+++ register/register.py
@@ -6,7 +6,6 @@ for i in range(26):
 
 
 with open("register.py.txt", "w") as f:
-    i = +3
     for i in range(26):
-        f.write("\n")
         f.write("".join([(chr((ord(c) + i - 65) % 26 + 65) if c != " " else " ") for c in l]))
+        f.write("\n\n")
