b1824051319d3d9345ce46e34df412a349b9c8ee
Park Jaehyung <explainpark101@gmail.com>
2023-09-21 15:50:48 +0900

    Lemur XOR finish

--- Challenges/01_General/10_LemurPNG/main.py
+++ Challenges/01_General/10_LemurPNG/main.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 
 flag = cv2.imread('flag.png')
-lemur = cv2.imread('flag.png')
-
-print(flag)
+lemur = cv2.imread('lemur.png')
+xored = np.bitwise_xor(flag, lemur)
+cv2.imwrite("result.png", xored)
