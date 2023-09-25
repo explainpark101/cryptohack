46acb3f9294c9d96f992be86f96f906875a56d25
Park Jaehyung <explainpark101@gmail.com>
2023-09-25 09:32:31 +0000

    extended gcd test

--- Challenges/01_General/12_extended_gcd/main.py
+++ Challenges/01_General/12_extended_gcd/main.py
@@ -1,16 +1,11 @@
-def gcd(a, b):
-    while a >= b:
-        a -= b
-    if a == 0: return b
-    return gcd(b, a)
+from math import prod
+
 
 
 def main():
     p = 26513
     q = 32321
     # p * u + q * v = gcd(p, q)
-    print(gcd(p, q))
-    ...
 
 if __name__ == '__main__':
     main()
